4e34cea946058fadc3edc5e79bcdaf07ba79b9e3
Savannah Rogers <savannah.a.rogers@gmail.com>
2024-05-16 18:57:50 -0700

    Update testing.R
    -bigger mesh
    -flexible plotting j and i
    -plot vis traps and pop
    -pdot plot

--- testing.R
+++ testing.R
@@ -2,30 +2,25 @@ library(secr)
 library(lubridate)
 
 Rcpp::sourceCpp("functions.cpp")
+source("movingdetectorlikelihood.R")
 
 ######data setup
 lambda0 = .9
 sigma = 300
 timeincr = 6*60 #specifies time increments for integration AND for distance matrix 
-meshgrid <- expand.grid(x = seq(000, 6000, 300), y = seq(000,6000, 300))
-mesh <- make.mask(meshgrid, buffer = 0, spacing = 500)
+meshgrid <- expand.grid(x = seq(0, 2600, 300), y = seq(0,2600, 300))
+mesh <- make.mask(meshgrid, buffer = 800, spacing = 500)
 D_mesh <- rep(.1, nrow(mesh))
 
 #traps
 #specify K moving traps
 #dataframe with column for trapid, x, y, and time
 trapsdf <- rbind(
-  data.frame(trapID = 1, x = seq(1500, 4500, 50), y = 1500, 
-             time = seq(ymd_hms("2024-01-01 8:00:00"), ymd_hms("2024-01-01 14:00:00"), 
+  data.frame(trapID = 1, x = seq(750, 1750, 250), y = 1000, 
+             time = seq(ymd_hms("2024-01-01 8:00:00"), (ymd_hms("2024-01-01 8:00:00") + (5-1)*360), 
                         by = 360)),
-  data.frame(trapID = 2, x = seq(1500, 4500, 50), y = 3500, 
-             time = seq(ymd_hms("2024-01-02 8:00:00"), ymd_hms("2024-01-02 14:00:00"), 
-                        by = 360)),
-  data.frame(trapID = 3, x = seq(1500, 4500, 50), y = 2500, 
-             time = seq(ymd_hms("2024-01-03 8:00:00"), ymd_hms("2024-01-03 14:00:00"), 
-                        by = 360)),
-  data.frame(trapID = 4, x = seq(1500, 4500, 50), y = 4500, 
-             time = seq(ymd_hms("2024-01-01 9:00:00"), ymd_hms("2024-01-01 15:00:00"), 
+  data.frame(trapID = 2, x = seq(750, 1750, 250), y = 1500, 
+             time = seq(ymd_hms("2024-01-02 8:00:00"), (ymd_hms("2024-01-02 8:00:00") + (5-1)*360), 
                         by = 360))
 )
 
@@ -35,7 +30,7 @@ pop <- sim.popn(D = D_mesh, core = mesh, model2D = "IHP",
 rownames(pop) <- NULL
 
 capthist <- sim_capthist(pop, trapsdf, timeincr, lambda0, sigma, D_mesh) #function in movingdetectorlikelihood.R
-
+dist_dat_pop <- create_distdat(trapsdf, pop)
 
 
 ########################################################################################################################################
@@ -44,46 +39,68 @@ capthist <- sim_capthist(pop, trapsdf, timeincr, lambda0, sigma, D_mesh) #functi
 ######################################################################################################
 library(raster)
 library(ggplot2)
-plotdat1 <- cbind(mesh, apply(as.array(0:(nrow(mesh)-1)), 1, FUN = function(x){haz_cpp(capthist[37,1], x, 0, lambda0, sigma, dist_dat)}))
+#must choose a ind/trap that has a detection
+ex_j <- 1
+ex_i <- min(which(!is.na(capthist[,ex_j])))
+plotdat1 <- cbind(mesh, apply(as.array(0:(nrow(mesh)-1)), 1, FUN = function(meshx){haz_cpp(capthist[ex_i,ex_j], meshx, (ex_j-1), lambda0, sigma, dist_dat)}))#trap 1
+
 colnames(plotdat1)[3] <- "hazard"
-plotdat1$survival <- apply(as.array(0:(nrow(mesh)-1)), 1, FUN = function(x){surv_cpp(dist_dat$times[1],capthist[37,1], timeincr/100,x, 0, lambda0, sigma, dist_dat)})
+plotdat1$survival <- apply(as.array(0:(nrow(mesh)-1)), 1, FUN = function(x){surv_cpp(dist_dat$times[1],capthist[ex_i,ex_j], timeincr/100,x, (ex_j-1), lambda0, sigma, dist_dat)})
 ggplot() +
   geom_raster(data = plotdat1,
               mapping = aes(x = x, y = y, fill = log(hazard))) +
-  geom_point(data = pop[37,], aes(x = x, y = y)) +
-  ggtitle(paste("log hazard for each mesh at time", capthist[37,1], "by trap 1"))
+  #trap 
+  geom_point(data = trapsdf[trapsdf$trapID == 1 & trapsdf$time == capthist[ex_i,ex_j],], aes(x = x, y = y), shape = "+") +
+  #an example simulated individual detected by this trap at this time
+  geom_point(data = pop[ex_i,], aes(x = x, y = y)) +
+  ggtitle(paste("log hazard for each mesh at time", capthist[ex_i,ex_j], "by trap ", ex_j))
 
 ggplot() +
   geom_raster(data = plotdat1,
               mapping = aes(x = x, y = y, fill = survival)) +
-  geom_point(data = pop[37,], aes(x = x, y = y)) + 
-  ggtitle(paste("Survival prob for each mesh at time", capthist[37,1], "by trap 1"))
+  #trap 
+  geom_point(data = trapsdf[trapsdf$trapID == 1 & trapsdf$time == capthist[ex_i,ex_j],], aes(x = x, y = y), shape = "+") +
+  #an example simulated individual detected by this trap at this time
+  geom_point(data = pop[ex_i,], aes(x = x, y = y)) +
+  ggtitle(paste("Survival prob for each mesh at time", capthist[ex_i,ex_j], "by trap", ex_j))
 
-plotdat2 <- data.frame(times = trapsdf[trapsdf$trapID == 1, "time"],
-                       haz = sapply(1:length(trapsdf[trapsdf$trapID == 1, "time"]),
-                                    FUN = function(i){
-                                      t = trapsdf[trapsdf$trapID == 1, "time"][i]
-                                      haz_cpp(t, 16, 0, lambda0, sigma, dist_dat) }),
-                       surv = sapply(1:length(trapsdf[trapsdf$trapID == 1, "time"]),
-                                     FUN = function(i){
-                                       t = trapsdf[trapsdf$trapID == 1, "time"][i]
-                                         surv_cpp(dist_dat$times[1], t, timeincr, 16, 0, lambda0, sigma, dist_dat)
+#find mesh pt closest to individual i (pop exi)
+dist_imesh <- do.call(rbind, apply(as.array(1:nrow(mesh)), 1, function(x)abs(pop[ex_i,]-mesh[x,])))
+dist_imesh_minx <- dist_imesh[dist_imesh$x == min(dist_imesh$x),]
+dist_imesh_minxy <- dist_imesh_minx[dist_imesh_minx$y == min(dist_imesh_minx$y),]
+closestmesh_m <- which(apply(as.array(1:nrow(dist_imesh)), 1, function(x)all(dist_imesh[x,] == dist_imesh_minxy)))
+
+plotdat2 <- data.frame(times = trapsdf[trapsdf$trapID == ex_j, "time"],
+                       haz = sapply(1:length(trapsdf[trapsdf$trapID == ex_j, "time"]),
+                                    FUN = function(timej){
+                                      t = trapsdf[trapsdf$trapID == ex_j, "time"][timej]
+                                      haz_cpp(t, closestmesh_m, (ex_j-1), lambda0, sigma, dist_dat) }),
+                       surv = sapply(1:length(trapsdf[trapsdf$trapID == ex_j, "time"]),
+                                     FUN = function(timej){
+                                       t = trapsdf[trapsdf$trapID == ex_j, "time"][timej]
+                                         surv_cpp(dist_dat$times[1], t, timeincr, closestmesh_m, (ex_j-1), lambda0, sigma, dist_dat)
                                      }),
-                       prob1stdett = sapply(1:length(trapsdf[trapsdf$trapID == 1, "time"]),
-                                            FUN = function(i){
-                                              t = trapsdf[trapsdf$trapID == 1, "time"][i]
-                                              haz_cpp(t, 16, 0, lambda0, sigma, dist_dat) *
-                                                surv_cpp(dist_dat$times[1], t, timeincr, 16, 0, lambda0, sigma, dist_dat)
+                       prob1stdett = sapply(1:length(trapsdf[trapsdf$trapID == ex_j, "time"]),
+                                            FUN = function(timej){
+                                              t = trapsdf[trapsdf$trapID == ex_j, "time"][timej]
+                                              haz_cpp(t, closestmesh_m, (ex_j-1), lambda0, sigma, dist_dat) *
+                                                surv_cpp(dist_dat$times[1], t, timeincr, closestmesh_m, (ex_j-1), lambda0, sigma, dist_dat)
                                             }))
 ggplot()+
-  geom_line(data = plotdat2, aes(x = times, y = haz))
+  geom_line(data = plotdat2, aes(x = times, y = haz)) +
+  geom_vline(xintercept = capthist[ex_i,ex_j], col = "blue")
 ggplot()+
-  geom_line(data = plotdat2, aes(x = times, y = log(surv)))
+  geom_line(data = plotdat2, aes(x = times, y = log(surv)))+
+  geom_vline(xintercept = capthist[ex_i,ex_j], col = "blue")
 ggplot()+
-  geom_line(data = plotdat2, aes(x = times, y = prob1stdett))
+  geom_line(data = plotdat2, aes(x = times, y = prob1stdett))+
+  geom_vline(xintercept = capthist[ex_i,ex_j], col = "blue")
 ggplot()+
-  geom_line(data = plotdat2, aes(x = times, y = log(prob1stdett)))
+  geom_line(data = plotdat2, aes(x = times, y = log(prob1stdett)))+
+  geom_vline(xintercept = capthist[ex_i,ex_j], col = "blue")
 
+
+#trying to minimize
 trylambda0s <- parallel::mclapply(as.list(seq(0,1,.1)),  FUN = function(lambda0_){
   negloglikelihood_cpp(lambda0_, sigma, D_mesh, timeincr, capthist, dist_dat)
 }, mc.cores = 4)
@@ -101,3 +118,45 @@ ggplot() +
   geom_point(data = plotsigmas, aes(x = sigma, y = llk))+
   geom_vline(xintercept = sigma)
 
+
+#check pdot
+pdot_m <- rep(NA, nrow(mesh))
+for (m in 1:nrow(mesh)){
+  surv_eachtrap <- rep(NA, nrow(dist_dat$traps)) 
+  for (trapk in 1:nrow(dist_dat$traps)){
+    opentimeindx <- which(!is.na(colSums(dist_dat$distmat[trapk,,], na.rm = F)))
+    topentime <- dist_dat$times[min(opentimeindx)]
+    tclosetime <- dist_dat$times[max(opentimeindx)]
+    
+    surv_eachtrap[trapk] <-  surv_cpp(topentime, tclosetime, timeincr, m-1, trapk-1, lambda0, sigma, dist_dat)
+  }
+  survalltraps = product(surv_eachtrap)
+  pdot = 1 - survalltraps
+  pdot_m[m] <- pdot
+}
+
+gettrapwcap <- function(trapk){
+  trapwcap <- trapsdf[trapsdf$trapID == trapk & as.numeric(trapsdf$time) %in% as.numeric(capthist[,trapk]),]
+  timecapcounts <- as.data.frame(table(as.numeric(capthist[,trapk])))
+  colnames(timecapcounts) <- c("timenum", "count")
+  trapwcap$numdets <- timecapcounts[timecapcounts$time == as.numeric(trapwcap$time),"count"]
+  return(trapwcap)
+}
+trapwcap <- do.call(rbind,
+                    apply(as.array(1:nrow(dist_dat$traps)), 1, 
+                          gettrapwcap))
+popcap <- cbind(pop, apply(capthist, 1, FUN = function(x){length(which(!is.na(x)))}))
+colnames(popcap)[3] <- "captures"
+ggplot() +
+  geom_point(data = cbind(mesh, pdot_m), aes(x = x, y = y, col = pdot_m)) +
+  geom_point(data = popcap, aes(x = x, y = y, size = as.factor(captures)), shape = 5) +
+  geom_point(data = trapwcap, aes(x = x, y = y#, col = numdets
+                                  ), size = 5) +
+  scale_color_viridis_c() +
+  
+  #May 16 so this all works sometimes, but not all the time. Sometimes, lambda0
+  #needs to be much smaller to minimize the neg llk. I think this means
+  #that in my simulation, I sometimes miss detections?
+  #but also sometimes it looks like a detection happens when the hazard is small
+  #and survival is high. Which is problematic
+  theme_classic()
