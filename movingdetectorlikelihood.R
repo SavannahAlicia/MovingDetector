7e0b6ae21130166000c6c8bae6ef990b80a3b090
Savannah Rogers <savannah.a.rogers@gmail.com>
2024-06-17 15:49:35 -0700

    Update movingdetectorlikelihood.R
    -comment functs in rcpp now
    -base 0 survcpp
    -omit commented old time seen probs

--- movingdetectorlikelihood.R
+++ movingdetectorlikelihood.R
@@ -2,171 +2,171 @@
 library(lubridate)
 library(secr)
 
-#' Calculate distance using dist_dat
-#'
-#' @param k index for trap
-#' @param x index for home range center (or mesh)
-#' @param t time as POSIXct object
-#' @param dist_dat list of "traps" (data frame of trap names), "mesh" (secr obj),
-#' "times" (POSIXct), and "distmat" (trap by mesh by times array)
-#' @param timesnap tolerance for difference between t and recorded times in 
-#' dist_dat, defaults to 10 minutes
-#'
-#' @return distance from distmat corresponding to k,x, and time closest to t
-#' @export
-distkxt <- function(k, x, t, dist_dat, timesnap = 10*60){
-  #referencing a distance matrix data object
-  distmat <- dist_dat$distmat
-  timediffs <- abs(difftime(dist_dat$times, t, units = "secs"))
-  closesttime <- which(timediffs == base::min(timediffs, na.rm = T))[1]
-  if(timediffs[closesttime] <= timesnap){
-    tindex <- closesttime
-  } else {
-    stop(paste("Closest time is more than", timesnap, "seconds away"))
-  }
-  distout <- distmat[k, x, tindex]
-  return(distout)
-}
-
-#' Hazard(d)
-#'
-#' @param lambda0 
-#' @param sigma
-#' @param d distance in m 
-#'
-#' @return detection rate
-#' @export
-hazdist <- function(lambda0, sigma, d){
-  lambda0 * exp(-(d^2/(2 * sigma^2)))
-}
-
-#' Hazard(t)
-#'
-#' @param t time index for dist_dat
-#' @param x home range center index for dist_dat
-#' @param k trap index for dist_dat
-#' @param lambda0 
-#' @param sigma 
-#' @param dist_dat list of "traps" (data frame of trap names), "mesh" (secr obj),
-#' "times" (POSIXct), and "distmat" (trap by mesh by times array)
-#'
-#' @return detection rate
-#' @export
-haz <- function(t, x, k, lambda0, sigma, dist_dat){
-  distkxt. <- distkxt(k = k, x = x, t = t, dist_dat)
-  hazout <- hazdist(lambda0 = lambda0, sigma = sigma, d = distkxt.)
-  return(hazout)
-}
-
-#' Survival Function
+#' #' Calculate distance using dist_dat
+#' #'
+#' #' @param k index for trap
+#' #' @param x index for home range center (or mesh)
+#' #' @param t time as POSIXct object
+#' #' @param dist_dat list of "traps" (data frame of trap names), "mesh" (secr obj),
+#' #' "times" (POSIXct), and "distmat" (trap by mesh by times array)
+#' #' @param timesnap tolerance for difference between t and recorded times in
+#' #' dist_dat, defaults to 10 minutes
+#' #'
+#' #' @return distance from distmat corresponding to k,x, and time closest to t
+#' #' @export
+#' distkxt <- function(k, x, t, dist_dat, timesnap = 10*60){
+#'   #referencing a distance matrix data object
+#'   distmat <- dist_dat$distmat
+#'   timediffs <- abs(difftime(dist_dat$times, t, units = "secs"))
+#'   closesttime <- which(timediffs == base::min(timediffs, na.rm = T))[1]
+#'   if(timediffs[closesttime] <= timesnap){
+#'     tindex <- closesttime
+#'   } else {
+#'     stop(paste("Closest time is more than", timesnap, "seconds away"))
+#'   }
+#'   distout <- distmat[k, x, tindex]
+#'   return(distout)
+#' }
 #' 
-#' @param timestart start time
-#' @param timeend end time
-#' @param timeincr increment of time used for integral (should be consistent)
-#' @param x home range center index for dist_dat
-#' @param k trap index for dist_dat
-#' @param lambda0 
-#' @param sigma 
-#' @param dist_dat list of "traps" (data frame of trap names), "mesh" (secr obj),
-#' "times" (POSIXct), and "distmat" (trap by mesh by times array)
+#' #' Hazard(d)
+#' #'
+#' #' @param lambda0
+#' #' @param sigma
+#' #' @param d distance in m
+#' #'
+#' #' @return detection rate
+#' #' @export
+#' hazdist <- function(lambda0, sigma, d){
+#'   lambda0 * exp(-(d^2/(2 * sigma^2)))
+#' }
 #' 
-#' @return probability home range center x is not detected by trap k from 
-#' timestart to timeend
-#' @export
-surv <- function(timestart, timeend, timeincr, x, k, lambda0, sigma, dist_dat){ 
-  #note time incr in seconds
-  if(timeend < timestart) {
-    survout <- NA
-  } else {
-    timesopen <- as.array(seq(from = timestart, to = timeend, by = timeincr))
-    hazs <- apply(as.array(1:length(timesopen)), 1, FUN = function(tt){
-      timet <- timesopen[tt]
-      haz(timet, x = x, k = k, lambda0 = lambda0, sigma = sigma, dist_dat = dist_dat)})
-    integral <- sum(hazs) * timeincr
-    survout <- exp(-integral)
-  }
-  return(survout)
-}
-
-#' Lambda_n (from likelihood)
+#' #' Hazard(t)
+#' #'
+#' #' @param t time index for dist_dat
+#' #' @param x home range center index for dist_dat
+#' #' @param k trap index for dist_dat
+#' #' @param lambda0
+#' #' @param sigma
+#' #' @param dist_dat list of "traps" (data frame of trap names), "mesh" (secr obj),
+#' #' "times" (POSIXct), and "distmat" (trap by mesh by times array)
+#' #'
+#' #' @return detection rate
+#' #' @export
+#' haz <- function(t, x, k, lambda0, sigma, dist_dat){
+#'   distkxt. <- distkxt(k = k, x = x, t = t, dist_dat)
+#'   hazout <- hazdist(lambda0 = lambda0, sigma = sigma, d = distkxt.)
+#'   return(hazout)
+#' }
 #' 
-#' @param dist_dat list of "traps" (data frame of trap names), "mesh" (secr obj),
-#' "times" (POSIXct), and "distmat" (trap by mesh by times array)
-#' @param D_mesh density at each mesh pt
-#' @param timeincr time increment for integral
-#' @param lambda0
-#' @param sigma 
+#' #' Survival Function
+#' #'
+#' #' @param timestart start time
+#' #' @param timeend end time
+#' #' @param timeincr increment of time used for integral (should be consistent)
+#' #' @param x home range center index for dist_dat
+#' #' @param k trap index for dist_dat
+#' #' @param lambda0
+#' #' @param sigma
+#' #' @param dist_dat list of "traps" (data frame of trap names), "mesh" (secr obj),
+#' #' "times" (POSIXct), and "distmat" (trap by mesh by times array)
+#' #'
+#' #' @return probability home range center x is not detected by trap k from
+#' #' timestart to timeend
+#' #' @export
+#' surv <- function(timestart, timeend, timeincr, x, k, lambda0, sigma, dist_dat){
+#'   #note time incr in seconds
+#'   if(timeend < timestart) {
+#'     survout <- NA
+#'   } else {
+#'     timesopen <- as.array(seq(from = timestart, to = timeend, by = timeincr))
+#'     hazs <- apply(as.array(1:length(timesopen)), 1, FUN = function(tt){
+#'       timet <- timesopen[tt]
+#'       haz(timet, x = x, k = k, lambda0 = lambda0, sigma = sigma, dist_dat = dist_dat)})
+#'     integral <- sum(hazs) * timeincr
+#'     survout <- exp(-integral)
+#'   }
+#'   return(survout)
+#' }
 #' 
-#' @return rate of detected home range centers
-#' @export
-lambdan <- function(dist_dat, D_mesh, timeincr, lambda0, sigma){
-  meshx_array <- as.array(1:nrow(dist_dat$mesh))
-  Dx_pdotxs <- apply(meshx_array, 1, FUN = function(meshx){
-    Dx <- D_mesh[meshx]
-    surv_eachtrap <- apply(as.array(1:nrow(dist_dat$traps)), 1, 
-                                    FUN = function(trapk){
-                                      opentimeindx <- which(!is.na(colSums(dist_dat$distmat[trapk,,], na.rm = F)))
-                                      topentime <- dist_dat$times[min(opentimeindx)]
-                                      tclosetime <- dist_dat$times[max(opentimeindx)]
-                                      surv(topentime, tclosetime, timeincr, 
-                                           x = meshx, k = trapk, 
-                                           lambda0 = lambda0, sigma = sigma,
-                                           dist_dat = dist_dat)}
-                          )
-    surv_alltraps <- prod(surv_eachtrap)
-    pdot <- 1 - surv_alltraps
-    Dx_pdotx <- Dx * pdot
-    return(Dx_pdotx)
-  })
-  integral <- sum(Dx_pdotxs) * attr(mesh, "area")
-  return(integral)
-}
-
-#' Negative log likelihood
+#' #' Lambda_n (from likelihood)
+#' #'
+#' #' @param dist_dat list of "traps" (data frame of trap names), "mesh" (secr obj),
+#' #' "times" (POSIXct), and "distmat" (trap by mesh by times array)
+#' #' @param D_mesh density at each mesh pt
+#' #' @param timeincr time increment for integral
+#' #' @param lambda0
+#' #' @param sigma
+#' #'
+#' #' @return rate of detected home range centers
+#' #' @export
+#' lambdan <- function(dist_dat, D_mesh, timeincr, lambda0, sigma){
+#'   meshx_array <- as.array(1:nrow(dist_dat$mesh))
+#'   Dx_pdotxs <- apply(meshx_array, 1, FUN = function(meshx){
+#'     Dx <- D_mesh[meshx]
+#'     surv_eachtrap <- apply(as.array(1:nrow(dist_dat$traps)), 1,
+#'                                     FUN = function(trapk){
+#'                                       opentimeindx <- which(!is.na(colSums(dist_dat$distmat[trapk,,], na.rm = F)))
+#'                                       topentime <- dist_dat$times[min(opentimeindx)]
+#'                                       tclosetime <- dist_dat$times[max(opentimeindx)]
+#'                                       surv(topentime, tclosetime, timeincr,
+#'                                            x = meshx, k = trapk,
+#'                                            lambda0 = lambda0, sigma = sigma,
+#'                                            dist_dat = dist_dat)}
+#'                           )
+#'     surv_alltraps <- prod(surv_eachtrap)
+#'     pdot <- 1 - surv_alltraps
+#'     Dx_pdotx <- Dx * pdot
+#'     return(Dx_pdotx)
+#'   })
+#'   integral <- sum(Dx_pdotxs) * attr(mesh, "area")
+#'   return(integral)
+#' }
 #' 
-#' @param lambda0 
-#' @param sigma 
-#' @param D_mesh density at each mesh pt
-#' @param timeincr increment of time used for integral (should be consistent)
-#' @param capthist capture history array of dimension individual by trap that 
-#' contains NA if not captured or time of first capture
-#' @param dist_dat list of "traps" (data frame of trap names), "mesh" (secr obj),
-#' "times" (POSIXct), and "distmat" (trap by mesh by times array)
-#' 
-#' @return negative log-likelihood value
-#' @export
-negloglikelihood <- function(lambda0, sigma, D_mesh, timeincr, capthist, dist_dat){
-  lambdan. <- lambdan(dist_dat, D_mesh, timeincr, lambda0, sigma)
-  n <- nrow(capthist)
-  integral_eachi <- apply(as.array(1:n), 1, FUN = function(i){
-    DKprod_eachx <- apply(as.array(1:nrow(dist_dat$mesh)), 1, FUN = function(x){
-      Sxhx_eachtrap <- 
-        apply(as.array(1:nrow(dist_dat$traps)), 1,
-              FUN = function(trapk){
-                opentimeindx <- which(!is.na(colSums(dist_dat$distmat[trapk,,], na.rm = F)))
-                starttime <- dist_dat$times[min(opentimeindx)]
-                if(!is.na(capthist[i,trapk])){
-                  dettime <- capthist[i, trapk]
-                  Sx <- surv(starttime, dettime, timeincr, x, trapk, lambda0, sigma, dist_dat)
-                  hx <- haz(dettime, x, trapk, lambda0, sigma, dist_dat)
-                  Sxhxout <- Sx * hx
-                } else { #individual never detected by this trap
-                  endtime <- dist_dat$times[max(opentimeindx)]
-                  Sx <- surv(starttime, endtime, timeincr, x, trapk, lambda0, sigma, dist_dat)
-                  Sxhxout <- Sx
-                }
-              })
-      Sxhx_alltraps <- prod(Sxhx_eachtrap)
-      DKprod_out <- D_mesh[x] * Sxhx_alltraps 
-      return(DKprod_out)
-    })
-    integral <- sum(DKprod_eachx) * attr(mesh, "area") #all mesh same size
-    return(integral)
-  })
-  lognfact <- sum(apply(as.array(0:(n-1)), 1, FUN = function(n_){ log(n-n_)}))
-  out <- -1 * (-lambdan. - lognfact + n * sum(log(integral_eachi)))
-  return(out)
-}
+#' #' Negative log likelihood
+#' #'
+#' #' @param lambda0
+#' #' @param sigma
+#' #' @param D_mesh density at each mesh pt
+#' #' @param timeincr increment of time used for integral (should be consistent)
+#' #' @param capthist capture history array of dimension individual by trap that
+#' #' contains NA if not captured or time of first capture
+#' #' @param dist_dat list of "traps" (data frame of trap names), "mesh" (secr obj),
+#' #' "times" (POSIXct), and "distmat" (trap by mesh by times array)
+#' #'
+#' #' @return negative log-likelihood value
+#' #' @export
+#' negloglikelihood <- function(lambda0, sigma, D_mesh, timeincr, capthist, dist_dat){
+#'   lambdan. <- lambdan(dist_dat, D_mesh, timeincr, lambda0, sigma)
+#'   n <- nrow(capthist)
+#'   integral_eachi <- apply(as.array(1:n), 1, FUN = function(i){
+#'     DKprod_eachx <- apply(as.array(1:nrow(dist_dat$mesh)), 1, FUN = function(x){
+#'       Sxhx_eachtrap <-
+#'         apply(as.array(1:nrow(dist_dat$traps)), 1,
+#'               FUN = function(trapk){
+#'                 opentimeindx <- which(!is.na(colSums(dist_dat$distmat[trapk,,], na.rm = F)))
+#'                 starttime <- dist_dat$times[min(opentimeindx)]
+#'                 if(!is.na(capthist[i,trapk])){
+#'                   dettime <- capthist[i, trapk]
+#'                   Sx <- surv(starttime, dettime, timeincr, x, trapk, lambda0, sigma, dist_dat)
+#'                   hx <- haz(dettime, x, trapk, lambda0, sigma, dist_dat)
+#'                   Sxhxout <- Sx * hx
+#'                 } else { #individual never detected by this trap
+#'                   endtime <- dist_dat$times[max(opentimeindx)]
+#'                   Sx <- surv(starttime, endtime, timeincr, x, trapk, lambda0, sigma, dist_dat)
+#'                   Sxhxout <- Sx
+#'                 }
+#'               })
+#'       Sxhx_alltraps <- prod(Sxhx_eachtrap)
+#'       DKprod_out <- D_mesh[x] * Sxhx_alltraps
+#'       return(DKprod_out)
+#'     })
+#'     integral <- sum(DKprod_eachx) * attr(mesh, "area") #all mesh same size
+#'     return(integral)
+#'   })
+#'   lognfact <- sum(apply(as.array(0:(n-1)), 1, FUN = function(n_){ log(n-n_)}))
+#'   out <- -1 * (-lambdan. - lognfact + n * sum(log(integral_eachi)))
+#'   return(out)
+#' }
 
 create_distdat <- function(traps, mesh){
   #if only one trap, create dummy trap to preserve matrix dimensions
@@ -222,6 +222,7 @@ create_distdat <- function(traps, mesh){
   return(dist_dat)
 }#see about using this in sim_capthist
 
+
 #' Simulate moving detector capture history
 #' 
 #' @param pop defaults to null, population locations from sim.popn
@@ -241,9 +242,9 @@ sim_capthist <- function(pop = NULL, traps, timeincrement, lambda0, sigma, D_mes
     rownames(pop) <- NULL
   }
   
-  dist_dat_pop <- create_distdat(traps, pop)
+  dist_dat_pop <- create_distdat(traps, pop)#this is now distances between pop hrcs and traps
 
-  capthist <- lapply(as.list(1:nrow(dist_dat_pop$mesh)), 
+  capthist <- lapply(as.list(1:nrow(dist_dat_pop$mesh)), #for each individual
                      FUN = function(hrcx){
     #capiks <- data.frame(ymd_hms(capik), nrow(dist_dat_pop$mesh), nrow(dist_dat_pop$traps))
     #for(hrcx in 1:nrow(dist_dat_pop$mesh)){
@@ -257,33 +258,28 @@ sim_capthist <- function(pop = NULL, traps, timeincrement, lambda0, sigma, D_mes
                                 opentimeindx <- which(!is.na(colSums(dist_dat_pop$distmat[trapk,,], na.rm = F)))
                                 topentime <- dist_dat_pop$times[min(opentimeindx)]
                                 tclosetime <- dist_dat_pop$times[max(opentimeindx)]
-                                probseenxk <- 1 - surv(topentime, tclosetime, timeincrement, hrcx, trapk, lambda0, sigma, dist_dat_pop)
+                                #prob individual is seen by this trap during the total time it's open
+                                probseenxk <- 1 - surv_cpp(topentime, tclosetime, timeincrement, (hrcx - 1), (trapk - 1), lambda0, sigma, dist_dat_pop) #any issue with indexing dist_dat_pop?
                                 seenxk_bool <- rbinom(1,1, probseenxk)
                                 if (seenxk_bool){
-                                  #this isn't quite right. instead,
+                                  #OPTION 1
                                   #uniformly sample between 0 and probability of survival over whole survey
                                   #then calculate the times associated with the probability
                                   #and that time is the time of detection
                                   #(uniformly sampling from CDF and back transforming)
-                                  #or just calculate 1-surv (prob of det) at discrete t's, then subtract
+                                  #OPTION 2 (selected)
+                                  # calculate 1-surv (prob of det) at discrete t's, then subtract
                                   #from each the 1-surv at t-1, then divide by 1-surv for the whole survey
                                   #and sample those
                                   timesopen <- seq(topentime, tclosetime, timeincrement)
                                   seenbyt <- apply(as.array(1:length(timesopen)), 1, FUN = function(t){
-                                    notseenuntil <- surv(topentime, timesopen[t], timeincrement, hrcx, trapk, lambda0, sigma, dist_dat_pop) + 1e-16
+                                    notseenuntil <- surv_cpp(topentime, timesopen[t], timeincrement, (hrcx-1), (trapk-1), lambda0, sigma, dist_dat_pop) + 1e-16
                                     return(1-notseenuntil)
                                     })
                                   seenbytminus1 <- c(0, seenbyt[1:(length(seenbyt)-1)])
                                   seenbetween_tminus1_and_t <- seenbyt - seenbytminus1
-                                  probseenbetween <- seenbetween_tminus1_and_t/ probseenxk
-                                  #detattime <- apply(as.array(1:length(timesopen)), 1, FUN = function(t){
-                                  #  #Note I need to log or something to deal with really small probs
-                                  #  notseenuntil <- surv(topentime, timesopen[t], timeincrement, hrcx, trapk, lambda0, sigma, dist_dat_pop) + 1e-16
-                                  #  seenat <- haz(timesopen[t], hrcx, trapk, lambda0, sigma, dist_dat_pop)
-                                  #  probdettime <- notseenuntil * seenat
-                                  #})
-                                  #capik <- timesopen[sample(length(detattime), 1, prob = detattime/sum(detattime))]
-                                  capik <- timesopen[sample(length(probseenbetween), 1, prob = probseenbetween)]
+                                  probseenbetween <- seenbetween_tminus1_and_t/ probseenxk #given you were seen in there somewhere
+                                  capik <- timesopen[sample(x = c(1:length(probseenbetween)), size = 1, replace = T,  prob = probseenbetween)]
                                   
                                 } else {
                                   capik <- ymd_hms(NA)
