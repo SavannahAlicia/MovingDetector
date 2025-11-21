cad436fcb29e0669fb2ea1cbe520aacb113f4f09
Savannah Rogers <savannah.a.rogers@gmail.com>
2025-11-21 14:58:09 -0800

    Update 00_functions_and_parameters.R
    larger for per km2

--- compare_moving_stat_2D/data_formatting/00_functions_and_parameters.R
+++ compare_moving_stat_2D/data_formatting/00_functions_and_parameters.R
@@ -16,8 +16,8 @@ lambda0 = .001
 sigma = 300
 beta1 <- -(1/40000)
 beta2 <- -2500
-beta3 <- 1.036
-flatD = 0.2
+beta3 <- 4.843736 #ensures that sum of Dmesh = sum Dmeshq
+flatD = 12 #per sqr km
 
 nsims = 50
 set.seed(1994)
