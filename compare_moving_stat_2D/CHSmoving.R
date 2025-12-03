7a26b4a80015c8dd4a7b344947ee9b20a204ee9d
Savannah Rogers <savannah.a.rogers@gmail.com>
2025-12-02 17:51:25 -0800

    Update CHSmoving.R
    induse from Cpp

--- compare_moving_stat_2D/CHSmoving.R
+++ compare_moving_stat_2D/CHSmoving.R
@@ -9,131 +9,11 @@ library(dplyr)
 library(tidyr)
 
 #-------------------------------functions---------------------------------------
-#' Create polygons for each box centered at grid pt
-#' 
-#' @param grid dataframe with x and y columns specifying grid pts
-#' @param spacing the spacing between grid pts, if not specified will calculate
-#' @return SpatialPolygons object in list form 
-#' @export
-create_grid_polygons <- function(grid, spacing = NULL){
-  #so we only have to create gridbboxes and lines list once
-  if(is.null(spacing)){
-    spacing <- min(dist(grid)[dist(grid$x)!=0])
-  }
-  #specify corners of box centered on grid pt
-  grid_spacing <- spacing
-  grid_bboxes <- data.frame(
-    left_bound = grid$x - (grid_spacing/2),
-    right_bound = grid$x + (grid_spacing/2), 
-    lower_bound = grid$y - (grid_spacing/2),
-    upper_bound = grid$y + (grid_spacing/2)
-  )
-  #create a polygon for box centered on grid pt
-  create_polygon_for_grid_row <- function(grid_bboxes_row){
-    upperleft <- as.numeric(c(grid_bboxes_row[1], grid_bboxes_row[4]))
-    lowerleft <- as.numeric(c(grid_bboxes_row[1], grid_bboxes_row[3]))
-    upperright <- as.numeric(c(grid_bboxes_row[2], grid_bboxes_row[4]))
-    lowerright <- as.numeric(c(grid_bboxes_row[2], grid_bboxes_row[3]))
-    boxpts <- rbind(upperleft, upperright, lowerright, lowerleft)
-    colnames(boxpts) <- c("x", "y")
-    rownames(boxpts) <- NULL
-    p = sp::Polygon(boxpts)
-    ps = sp::Polygons(list(p),1)
-    sps = sp::SpatialPolygons(list(ps))
-    proj4string(sps) <- sp::CRS(sf::st_crs(26916)$input)
-    return(sps)
-  }
-  #create polygon box for all grid pts
-  grid_bboxes$polygons  <- apply(X = grid_bboxes, MARGIN = 1, 
-                                 FUN = create_polygon_for_grid_row)
-  return(grid_bboxes)
-}
 
-#' Create named list of SpatialLines where name is track ID (occ)
-#' @param tracksdf dataframe with xy, occ, trapno, and time
-#' @return named list of SpatialLines objects
-#' @export
-create_line_spatlines <- function(tracksdf, scenario = "onison", 
-                                  tracksdfcolname = "ID",
-                                  projto = sp::CRS(sf::st_crs(26916)$input)){
-  #create a line for between each pt of track, put into named list by ID
-  #check that it's not the last point in a track
-  #(we don't want a line between tracks)
-  check_pt <- function(row, tracksdf. = tracksdf){
-    if (tracksdf.[row, tracksdfcolname] == tracksdf.[row+1, tracksdfcolname]){
-      newline <- Line(tracksdf.[c(row, row+1),c("x", "y")])
-    } else {
-      newline <- NULL
-    }
-    return(newline)
-  }
-  linelist <- lapply(X = as.list(1:(nrow(tracksdf)-1)), FUN = check_pt)
-  names(linelist) <- tracksdf[1:(nrow(tracksdf)-1), tracksdfcolname]
-  linelist <- linelist[which(!lapply(linelist, is.null) == TRUE)]
-  big_Lines_list <- list()
-  nocc <- length(unique(tracksdf[, tracksdfcolname]))
-  for (trackindex in 1:nocc){
-    trackIDi <- unique(tracksdf[, tracksdfcolname])[trackindex]
-    thetrack <- tracksdf[which(tracksdf[, tracksdfcolname] == trackIDi),]
-    thelines <- linelist[which(names(linelist) == paste(trackIDi))]
-    
-    #check scenario
-    if (scenario == "onison"){ #on effort is on effort
-      newlines <- list(Lines(thelines[which(
-        thetrack[1:(nrow(thetrack)-1),]$effort == "OnEffort")], 
-        ID = paste(trackIDi)))
-    } else if (scenario == "everything"){ #everything is on effort
-      newlines <- list(Lines(thelines, ID = paste(trackIDi)))
-    }
-    #just creating desired class of object
-    newlines_spat <- SpatialLines(newlines)
-    #specify projection
-    proj4string(newlines_spat) <- projto
-    #adding new objects to list
-    big_Lines_list <- append(big_Lines_list, newlines_spat)
-    #naming list for track ID
-    names(big_Lines_list)[trackindex] <- paste(trackIDi)
-  }
-  return(big_Lines_list)
-}
+setwd("~/Documents/UniStAndrews/MovingDetector")
+Rcpp::sourceCpp("approx_movingdetectorlikelihood.cpp")
+setwd("~/Documents/UniStAndrews/Dolphins/Charleston")
 
-#' Get length line that cross polygon
-#' @param polygon a spatial polygon object
-#' @param sp_line a Spatial Lines object
-#' @return returns the length of the line within polygon
-#' @export
-get_length <- function(polygon, sp_line){
-  boxlength = 0
-  #attempts to chop line to just the bit within the polygon
-  theintersection <- st_intersection(st_as_sf(polygon), st_as_sf(sp_line))
-  #only calculate length of the line if there is a line that exists
-  if(length(theintersection$geometry) > 0){
-    boxlength <- as.numeric(st_length(theintersection))
-  } #otherwise the length remains 0
-  return(boxlength)
-}
-
-#'Calculate track lengths in grid boxes
-#' function to find intersection between lines and grid box 
-#' @param tracks_sp_lines list of lines labelled by track ID in name field
-#' @param grid_polygons dataframe with a polygon in each row corresponding to 
-#'                      that grid pt
-#' @return column of summed intersections, each row is grid pt (trap)
-#'         and sum is for all tracks specified in function
-lengths_in_grid <- function(tracks_sp_lines, tracksinoc, grid_polygons){
-  trackspl_to_use <- tracks_sp_lines[which(names(tracks_sp_lines) %in%
-                                             as.character(tracksinoc))]
-  
-  if (length(trackspl_to_use) == 1){#just one track per occ now 
-    inters <-  sapply(X = grid_polygons$polygons, 
-                      FUN = function(x){get_length(x, trackspl_to_use[[1]])})
-  } else {
-    warning(paste("track length", paste0(tracksinoc, collapse = " "), "not equal to 1"))
-    inters = rep(NA, length(grid_polygons$polygons))
-  }
-  return(inters)
-}
-#
 #-------------Read files -------------------------------------------------------
 
 lpoly <- readRDS("data/all_scenarios/larger_poly.Rds")
@@ -247,39 +127,7 @@ cht <- apply(as.array(unique(sight_single$ID)), 1,
 ch_t <- aperm(array(cht, dim = c(nrow(traps), nocc, length(unique(sight_single$ID)))),
                c(3,2,1))
 
-trapcells <- create_grid_polygons(trapscr, spacing = 2000)
-#use
-#for each individual, create a use matrix
-create_ind_use <- function(ch_t, trapcells, tracksdf){
-  use <-mclapply(as.list(1:dim(ch_t)[1]), FUN = function(i){
-    apply(as.array(1:dim(ch_t)[2]), 1, function(k){
-      trackoccdf <- tracksdf[tracksdf$occ == k,]
-      if(!all(is.na(ch_t[i,k,]))){
-        #if i detected k, discard survey pts after detection in occasion k
-        dettime <- ch_t[i,k,which(!is.na(ch_t[i,k,]))]
-        trackoccdf <- trackoccdf[trackoccdf$time <= dettime,]
-        #add up length of trackline in each grid cell
-        useik <- lengths_in_grid(create_line_spatlines(trackoccdf, tracksdfcolname = "occ"), k, trapcells)
-      } else {#if i wasn't detected in k, all traps used full amount, so skip subsetting
-        useik <- usage(trapscr)[,k]
-      }
-      #(if there are covariates, perhaps could create matrix for them here, but for now ignore)
-    })
-    
-  }, mc.cores = 3)
-  return(use)
-}
-
-trapcells <- create_grid_polygons(trapscr, spacing = 2000)
-
-# induse_ls <- create_ind_use(ch_t, trapcells, tracksdf)
-# induse <- aperm(
-#   array(unlist(induse_ls), 
-#         dim =c(nrow(traps), nocc, dim(capthist)[1])), 
-#   c(3, 1, 2))
-# saveRDS(induse, "~/Documents/UniStAndrews/MovingDetector/compare_moving_stat_2D/CHS_results/induse.Rds")
-induse <- readRDS("~/Documents/UniStAndrews/MovingDetector/compare_moving_stat_2D/CHS_results/induse.Rds")
-
+induse <- create_ind_use_C(ch_t, trapscr, 2000, tracksdf, scenario = "onison")
 #convert capthist back to 1s and 0s
 ch_10 <- ch_t
 ch_10[is.na(ch_10)] <- 0
@@ -478,10 +326,6 @@ move_fit <- function(capthist,
 }
 
 
-
-setwd("~/Documents/UniStAndrews/MovingDetector")
-Rcpp::sourceCpp("approx_movingdetectorlikelihood.cpp")
-setwd("~/Documents/UniStAndrews/Dolphins/Charleston")
 
 #soap film smoother
 cut <- list(matrix(c(1143961, bbox(all_poly_2000)[2,1], #anticlockwise, start bottom left
