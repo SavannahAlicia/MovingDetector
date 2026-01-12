2b90ff680fa0c4a33d475464489141301049de77
Savannah Rogers <savannah.a.rogers@gmail.com>
2026-01-11 19:30:29 -0800

    different parallel for cluster

--- compare_moving_stat_2D/data_formatting/03_run_save_fits.R
+++ compare_moving_stat_2D/data_formatting/03_run_save_fits.R
@@ -1,44 +1,55 @@
 #fit multiple models and save output
+ncores <- 30
+cl <- makeCluster(ncores)
 
+# Load packages on workers
+clusterEvalQ(cl, library(Rcpp))
+
+# Export all objects/functions needed by sim_fit
+clusterExport(cl, varlist = c(
+  "sim_fit", "simulate_popandcapthist", "fit_capthist",
+  "traps", "tracksdf", "mesh", "meshspacing",
+  "dist_trapmesh", "useall", "lambda0", "sigma",
+  "D_mesh_v", "D_mesh_f", "beta1", "beta2", "N", "hazdenom"
+))
+
+# Run first batch (inhomogeneous)
 start.time.all_q <- Sys.time()
-all_sim_fits_q <- mclapply(X = as.list(1:nsims),
-                           FUN = function(sim){
-                             return(sim_fit(traps,
-                                            tracksdf, 
-                                            mesh, 
-                                            meshspacing,
-                                            dist_trapmesh,
-                                            useall,
-                                            lambda0, sigma, D_mesh_v, 
-                                            beta1, beta2, N,
-                                            hazdenom, 
-                                            Dmod = "~x^2"))
-                           },
-                           mc.cores = 30
-)
+all_sim_fits_q <- parLapply(cl, 1:nsims, function(sim) {
+  tryCatch({
+    sim_fit(traps, tracksdf, mesh, meshspacing,
+            dist_trapmesh, useall,
+            lambda0, sigma, D_mesh_v, 
+            beta1, beta2, N,
+            hazdenom, 
+            Dmod = "~x^2")
+  }, error = function(e) {
+    message("Simulation ", sim, " failed: ", e$message)
+    return(NULL)
+  })
+})
 tot.time.all_q <- difftime(Sys.time(), start.time.all_q, units = "secs")
 print(tot.time.all_q)
 
-
+# Run second batch (~1)
 start.time.all1 <- Sys.time()
-all_sim_fits <- mclapply(X = as.list(1:nsims),
-                          FUN = function(sim){
-                            return(sim_fit(traps,
-                                           tracksdf, 
-                                           mesh, 
-                                           meshspacing,
-                                           dist_trapmesh,
-                                           useall,
-                                           lambda0, sigma, D_mesh_f, 
-                                           beta1, beta2, N,
-                                           hazdenom,
-                                           Dmod = "~1"))
-                          },
-                          mc.cores = 30
-)
+all_sim_fits <- parLapply(cl, 1:nsims, function(sim) {
+  tryCatch({
+    sim_fit(traps, tracksdf, mesh, meshspacing,
+            dist_trapmesh, useall,
+            lambda0, sigma, D_mesh_f, 
+            beta1, beta2, N,
+            hazdenom,
+            Dmod = "~1")
+  }, error = function(e) {
+    message("Simulation ", sim, " failed: ", e$message)
+    return(NULL)
+  })
+})
 tot.time.all1 <- difftime(Sys.time(), start.time.all1, units = "secs")
 print(tot.time.all1)
 
+stopCluster(cl)
 
 
 saveRDS(all_sim_fits_q, paste(dirstart,"variable_dens.Rds", sep = ""))
